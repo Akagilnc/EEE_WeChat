07d3c153a9dcbbddd4b19293e52f65e6b8dbae80
DeronLee <akagilnc@gmail.com>
2019-02-28 23:57:47 +0900

    add winnie to answer 2

--- index.py
+++ index.py
@@ -13,8 +13,8 @@ def hello(message):
 def hello(message):
     if message.content.lower() in ['hello', '你好', '好', 'hi']:
         return "Hello wechat world，你好欢迎来到微信的世界"
-    if message.content.lower() == "ak":
-        return "Ak loves Winnie"
+    if message.content.lower() == ['ak', '猩猩', '李胖娃', '胖娃', '胖娃儿']:
+        return "{} loves Winnie".format(message.content)
     return "主人正在努力挖土烧砖盖楼 \n内容很快就来 \n不要着急 \n休息 \n休息一下"
 
 
