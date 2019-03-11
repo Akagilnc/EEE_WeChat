f71f7cf1b868c18a2c7d72651056ae45ff58dd8c
DeronLee <akagilnc@gmail.com>
2019-03-11 15:59:57 +0900

    update image address2

--- index.py
+++ index.py
@@ -23,7 +23,7 @@ def hello(message):
         reply = ArticlesReply(message=message)
         article = Article(title="课表",
                           description="3E女子商学院课表",
-                          img='''https://github.com/Akagilnc/Landing_Page_3EWBS/blob/master/img/3e_logo.jpeg''',
+                          img='''https://github.com/Akagilnc/Landing_Page_3EWBS/blob/master/img/3E.JPG''',
                           url='''http://www.3ewbs.com/class_schedule''')
         reply.add_article(article)
         return reply
