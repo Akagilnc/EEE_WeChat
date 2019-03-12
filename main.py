d33ca3121b0797601fd455601f2c6f74b489a6f7
DeronLee <akagilnc@gmail.com>
2019-03-12 19:11:05 +0900

    try to add https method

--- main.py
+++ main.py
@@ -4,7 +4,34 @@ from flask import Flask
 from werobot.contrib.flask import make_view
 from index import robot
 
+
+class HTTPMethodOverrideMiddleware(object):
+    allowed_methods = frozenset([
+        'GET',
+        'HEAD',
+        'POST',
+        'DELETE',
+        'PUT',
+        'PATCH',
+        'OPTIONS'
+    ])
+    bodyless_methods = frozenset(['GET', 'HEAD', 'OPTIONS', 'DELETE'])
+
+    def __init__(self, app):
+        self.app = app
+
+    def __call__(self, environ, start_response):
+        method = environ.get('HTTP_X_HTTP_METHOD_OVERRIDE', '').upper()
+        if method in self.allowed_methods:
+            method = method.encode('ascii', 'replace')
+            environ['REQUEST_METHOD'] = method
+        if method in self.bodyless_methods:
+            environ['CONTENT_LENGTH'] = '0'
+        return self.app(environ, start_response)
+
+
 app = Flask(__name__)
+app.wsgi_app = HTTPMethodOverrideMiddleware(app.wsgi_app)
 app.add_url_rule(rule='/robot',  # WeRoBot 挂载地址
                  endpoint='robot',  # Flask 的 endpoint
                  view_func=make_view(robot),
